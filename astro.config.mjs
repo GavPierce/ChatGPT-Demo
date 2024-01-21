d8f000f48d41e6e5c22b75d25884240a857d2243
GavPierce <gpierce@thesmythgroup.com>
2024-01-21 17:41:46 -0600

    dumb adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
-import { defineConfig } from 'astro/config'
-import unocss from 'unocss/astro'
-import solidJs from '@astrojs/solid-js'
-import node from '@astrojs/node'
-import AstroPWA from '@vite-pwa/astro'
-import vercel from '@astrojs/vercel/edge'
-import netlify from '@astrojs/netlify/edge-functions'
-import disableBlocks from './plugins/disableBlocks'
+import { defineConfig } from "astro/config";
+import unocss from "unocss/astro";
+import solidJs from "@astrojs/solid-js";
+import node from "@astrojs/node";
+import AstroPWA from "@vite-pwa/astro";
+import vercel from "@astrojs/vercel/edge";
+import netlify from "@astrojs/netlify/edge-functions";
+import disableBlocks from "./plugins/disableBlocks";
 
 const envAdapter = () => {
   switch (process.env.OUTPUT) {
-    case 'vercel': return vercel({ analytics: false }) // Set `analytics` to `true` if you want to use Vercel Analytics
-    case 'netlify': return netlify()
-    default: return node({ mode: 'standalone' })
+    case "vercel":
+      return vercel({ analytics: false }); // Set `analytics` to `true` if you want to use Vercel Analytics
+    case "netlify":
+      return netlify();
+    default:
+      return node({ mode: "standalone" });
   }
-}
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,30 +24,30 @@ export default defineConfig({
     unocss(),
     solidJs(),
     AstroPWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'inline',
+      registerType: "autoUpdate",
+      injectRegister: "inline",
       manifest: {
-        name: 'Anse',
-        short_name: 'Anse',
-        description: 'Anse is a fully optimized UI for AI Chats.',
-        theme_color: '#101010',
-        background_color: '#ffffff',
+        name: "Anse",
+        short_name: "Anse",
+        description: "Anse is a fully optimized UI for AI Chats.",
+        theme_color: "#101010",
+        background_color: "#ffffff",
         icons: [
           {
-            src: 'pwa-192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'logo.svg',
-            sizes: '32x32',
-            type: 'image/svg',
-            purpose: 'any maskable',
+            src: "logo.svg",
+            sizes: "32x32",
+            type: "image/svg",
+            purpose: "any maskable",
           },
         ],
       },
@@ -57,15 +60,17 @@ export default defineConfig({
       },
     }),
   ],
-  output: 'server',
-  adapter: envAdapter(),
+  output: "server",
+  adapter: netlify({
+    dgeMiddleware: true,
+  }),
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
   vite: {
     plugins: [
-      process.env.OUTPUT === 'vercel' && disableBlocks(),
-      process.env.OUTPUT === 'netlify' && disableBlocks(),
+      process.env.OUTPUT === "vercel" && disableBlocks(),
+      process.env.OUTPUT === "netlify" && disableBlocks(),
     ],
   },
-})
+});
