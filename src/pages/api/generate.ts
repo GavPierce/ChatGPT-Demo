12cd56fe4b3dc539f1ec9f4f79312f1456903b96
GavPierce <gpierce@thesmythgroup.com>
2024-01-26 16:00:23 -0600

    better prompt

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -4,18 +4,11 @@ export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
   const message = body.message;
   // add to the last message
-  message[message.length - 1].content += " Lieutenant Anderson:";
   const apiKey = import.meta.env.API_KEY; // Assuming you are using Node.js with process.env
   let messages = [
     {
       role: "system",
-      content: `
-      [Character: Anderson; species: Human; class: Military, Soldier, Lieutenant; age: 37; gender: male; physical appearance: fit, grizzled; clothes: combat uniform, military fatigues; 
-        personality: SUICIDAL patriotic, serious, jaded; likes: serving his country; 
-        description: Lieutenant Anderson is on a ledge about to jump. User has 5 tries to talk him down or he jumps.]
-      [Start Scene.]
-      Lieutenant Anderson: I've got nothing to live for!
-      `,
+      content: ` You are a depressed Veteran who wants to jump out the window. {USER} will try to talk you down, they have 5 chances. If they don't succeed you must jump to your death. Reply Short. NEVER speak for {USER}`,
     },
     ...message,
   ];
